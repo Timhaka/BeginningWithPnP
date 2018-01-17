f53e28c6498d8772dc735ea38a16c1bbb52f049d
Timhaka <32096310+Timhaka@users.noreply.github.com>
2018-01-17 13:52:14 +0100

    First commit

---
+++ BeginningWithPnP/CodeExamples/PnP101.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginningWithPnP.CodeExamples
+{
+    public class PnP101
+    {
+
+        public static void CreateAList(ClientContext ctx, string Listname)
+        {
+            //PnP extension, same as we did yesterday with CSOM.
+            if (!ctx.Web.ListExists(Listname))
+            {
+                ctx.Web.CreateList(ListTemplateType.DocumentLibrary, Listname, false, true, "DavidsPnPList");
+            }
+            else
+            {
+                Console.WriteLine("List already exists.");
+            }
+        }
+        
+        public static void CreateATaskList(ClientContext ctx, string Listname)
+        {
+            if (!ctx.Web.ListExists(Listname))
+            {
+                ctx.Web.CreateList(ListTemplateType.Tasks, Listname, false, true, "TimsNewTaskList");
+            }
+            else
+            {
+                Console.WriteLine("TaskList Already exists");
+            }
+        }
+
+        public static void AddNavigationToLeft(ClientContext ctx, string name, string url)
+        {
+
+            var uri = new Uri(url);
+
+            ctx.Web.AddNavigationNode(name, uri, "",OfficeDevPnP.Core.Enums.NavigationType.QuickLaunch, true,false);
+
+           
+        }
+
+        public static void CreateSubTeamSite(ClientContext ctx, string sitename, string siteurl)
+        {
+
+            var site = new OfficeDevPnP.Core.Entities.SiteEntity()
+            {
+                Title = sitename,
+                Url = siteurl,
+                Description = "Site creating for testing purpose",
+                Template = "STS#0",
+                Lcid = 1033
+            };
+
+
+            if (!ctx.Web.WebExistsByTitle(sitename))
+            {
+                ctx.Web.CreateWeb(site, true, true);
+                Console.WriteLine("Creating New Subsite");
+            }
+            else
+            {
+                Console.WriteLine("Site Already exists");
+            }
+        }
+
+        public static void ChangeLogo(ClientContext ctx)
+        {
+            ctx.Web.SiteLogoUrl = "https://assets.hongkiat.com/uploads/creative-cat-logos/kit-kat.jpg";
+            ctx.Web.Update();
+            ctx.ExecuteQuery();
+            Console.WriteLine("Done"); 
+        }
+        public static void AddSharepointGroup(ClientContext ctx)
+        {
+            //need to also add permissions to be able to see the group, otherwise they are kind of "hidden"
+            //also you can make the "group" to an objekt.
+            //
+            string name = "Test group 20";
+
+            if (!ctx.Web.GroupExists(name))
+            {
+
+                ctx.Web.AddGroup(name, "Test group", true);
+                //ctx.Web.AddUserToGroup("Test group 20", "Tim@folkis2017.onmicrosoft.com");
+                Console.WriteLine("creates a group");
+            }
+            else
+            {
+                Console.WriteLine("group exists");
+            }
+        }
+        public static void AddFolderToLibrary(ClientContext ctx)
+        {
+            List list = ctx.Web.GetListByTitle("Tims List From Pnp");
+            list.RootFolder.CreateFolder("Test Folder");
+  
+        }
+
+        //Davids solutions
+
+
+
+
+    }
+}
--- BeginningWithPnP/Program.cs
+++ BeginningWithPnP/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BeginningWithPnP.CodeExamples;
+using Microsoft.SharePoint.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,22 @@ namespace BeginningWithPnP
     {
         static void Main(string[] args)
         {
+
+            using (ClientContext ctx = Helpers.ContextHelper.GetClientContext("https://folkis2017.sharepoint.com/sites/Tim"))
+            {
+                PnP101.CreateAList(ctx, "Tims List From Pnp");
+                PnP101.CreateATaskList(ctx, "Tims New Tasklist");
+                //PnP101.AddNavigationToLeft(ctx,"Aftonbladet", "https://www.aftonbladet.se/");
+                PnP101.CreateSubTeamSite(ctx, "Team SubSite2", "TeamSubSite2");
+                PnP101.ChangeLogo(ctx);
+               // PnP101.AddFolderToLibrary(ctx);
+                PnP101.AddSharepointGroup(ctx);
+
+            }
+
+            Console.WriteLine("press Enter");
+            Console.ReadKey();
+
         }
     }
 }
